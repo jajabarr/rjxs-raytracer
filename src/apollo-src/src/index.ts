fc01cbeff07f5e0c75e439bcd409b442ad1c199d
jajabarr <jajabarr@umich.edu>
2020-08-15 21:39:10 -0400

    update, remove apollo code

--- src/apollo-src/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Auth } from './auth';
-export { Build } from './builds';
--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import { Auth } from '../apollo-src/src/auth';
-import logo from './logo.svg';
-import styles from './app.module.scss';
+import React from "react";
+import styles from "./app.module.scss";
 
 const App = () => {
   return <div className={styles.app}>hello</div>;
